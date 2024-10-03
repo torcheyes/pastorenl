8ab711e7847b5733a5807235d95e2661b7fb3780
dvhsh <david@dvh.sh>
2024-10-02 23:57:16 -0700

    remove ununsed imports

--- src/containers/Landing/Brands.tsx
+++ src/containers/Landing/Brands.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { ParallaxLogos } from "@components/ParallaxLogos";
 
 import { Beyma } from "@components/svg/brands/beyma";
